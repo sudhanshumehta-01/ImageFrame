b38908b23b4ae7eb45e98e8dd031857787ab5ba4
sudha <sudha@192.168.1.4>
2025-01-11 21:11:45 +0530

    First Commit!

---
+++ src/main/java/error/mavenproject2/AddImage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.mavenproject2;
+
+/**
+ *
+ * @author sudha
+ */
+public class AddImage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
